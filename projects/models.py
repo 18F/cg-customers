f31722dbfb38419c5acc27e69fea2d0ab6263662
James C. Scott <jamesc.scott@gsa.gov>
2017-05-24 13:15:36 -0400

    add project comment

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+from customer_admin.models import Comment, CommentInline
 from django.contrib import admin
 from django.core import urlresolvers
 from django.db import models
@@ -18,8 +19,15 @@ class Project(models.Model):
     def __str__(self):
         return 'Project: {}'.format(self.project_name)
 
+class ProjectComment(Comment):
+    project = models.ForeignKey(Project)
+
+class ProjectCommentInline(CommentInline):
+    model = ProjectComment
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('project_name', 'agency', 'system_owner', 'iaa_link', 'package_link',)
+    inlines = [ProjectCommentInline]
     def iaa_link(self, obj):
         link = urlresolvers.reverse('admin:iaa_iaa_change', args=[obj.iaa_id])
         return format_html('<a target="_blank" href="{}">{}</a>', link, obj.iaa) if obj.iaa else None
