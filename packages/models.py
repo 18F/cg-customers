bf3dab4f6851c67d36b0a5de8273bdf35bfbf24a
James C. Scott <jamesc.scott@gsa.gov>
2017-06-07 09:29:19 -0400

    Make adjustments per Britta's feedback
    Have a list of package and quota history
    Each history should have a start and end date
    Also, change the package price from yearly to monthly

--- packages/models.py
+++ packages/models.py
@@ -3,7 +3,7 @@ from djmoney.models.fields import MoneyField
 
 class Package(models.Model):
     package_name = models.CharField(max_length=64)
-    yearly_price = MoneyField(max_digits=12, decimal_places=2, default_currency='USD')
+    monthly_price = MoneyField(max_digits=12, decimal_places=2, default_currency='USD')
 
     def __str__(self):
         return 'Package: {}'.format(self.package_name)
